c41d648d47941920746644d08c601c6cad0cd67e
stsh1 <40880676+stsh1@users.noreply.github.com>
2018-07-23 15:56:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,5 @@
 <?php
-//including the database connection file
-//include_once("config.php");
-try {
-    $dbConn = new PDO("sqlsrv:server = tcp:finalprserver.database.windows.net,1433; Database = finalprdb", "ravdeep", "Like@1234");
-    $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "ravdeep@finalprserver", "pwd" => "Like@1234", "Database" => "finalprdb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:finalprserver.database.windows.net,1433";
-$dbConn = sqlsrv_connect($serverName, $connectionInfo);
+include_once("config.php");
 
 //fetching data in descending order (lastest entry first)
 $result = $dbConn->query("SELECT * FROM users ORDER BY id DESC");
