f7f918daf62aa1a047e2e32ddeab15c40168d418
stsh1 <40880676+stsh1@users.noreply.github.com>
2018-07-23 14:55:50 +0530

    Update config.php

--- config.php
+++ config.php
@@ -4,6 +4,7 @@
 try {
     $dbConn = new PDO("sqlsrv:server = tcp:finalprserver.database.windows.net,1433; Database = finalprdb", "ravdeep", "Like@1234");
     $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "connection establish";
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
