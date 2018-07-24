c07330bfdab71a1808315c9b8230a7f5d4ab615d
stsh1 <40880676+stsh1@users.noreply.github.com>
2018-07-24 14:54:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
-$conn=mysql_connect(" tcp:finalprserver.database.windows.net","ravdeep","Like@1234");
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:finalprserver.database.windows.net,1433; Database = finalprdbb", "ravdeep", "Like@1234");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "ravdeep@finalprserver", "pwd" => "Like@1234", "Database" => "finalprdbb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:finalprserver.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
 $q="Select * from u_reg";
 $run=mysqli_query($conn, $q);
 	 
