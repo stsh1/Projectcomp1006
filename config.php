8c2cd0f4c6ad4852561024345dae329ba672c0d7
stsh1 <40880676+stsh1@users.noreply.github.com>
2018-07-24 12:50:33 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
-$conn=mysql_connect("localhost","root","");
+$conn=mysql_connect(" tcp:finalprserver.database.windows.net","ravdeep","Like@1234");
 				$db=mysql_select_db("students",$conn);
 ?>
