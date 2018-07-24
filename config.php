7ce387b743a30e467afdec9c696c344d06a25602
stsh1 <40880676+stsh1@users.noreply.github.com>
2018-07-24 13:16:46 +0530

    Update config.php

--- config.php
+++ config.php
@@ -10,6 +10,6 @@ $sqlcreateTable ="CREATE TABLE IF NOT EXISTS u_reg (
 id INT(6) AUTO_INCREMENT PRIMARY KEY, 
 name VARCHAR(20) NOT NULL,
 fname VARCHAR(20) NOT NULL,
-school VARCHAR(20),rollno VARCHAR(20),class VARCHAR(20)";
+school VARCHAR(20),rollno VARCHAR(20),class VARCHAR(20))";
 mysqli_query($conn, $sqlcreateTable);
 ?>
