24cdbaf7abbfec5c9150b1dfbdc071f03714cd59
stsh1 <40880676+stsh1@users.noreply.github.com>
2018-07-24 10:52:09 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,7 @@
 <?php
-
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $dbConn = new PDO("sqlsrv:server = tcp:finalprserver.database.windows.net,1433; Database = finalprdb", "ravdeep", "Like@1234");
+    $dbConn = new PDO("sqlsrv:server = tcp:finalprserver.database.windows.net,1433; Database = finalprdbb", "ravdeep", "Like@1234");
     $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -11,7 +10,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "ravdeep@finalprserver", "pwd" => "Like@1234", "Database" => "finalprdb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "ravdeep@finalprserver", "pwd" => "Like@1234", "Database" => "finalprdbb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:finalprserver.database.windows.net,1433";
 $dbConn = sqlsrv_connect($serverName, $connectionInfo);
 
