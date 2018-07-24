9c8f303152c0eeb4888bc3813e33b3fd3ad955f9
stsh1 <40880676+stsh1@users.noreply.github.com>
2018-07-24 13:10:11 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,15 @@
 <?php
 $conn=mysql_connect(" tcp:finalprserver.database.windows.net","ravdeep","Like@1234");
 				$db=mysql_select_db("students",$conn);
+
+
+
+
+
+$sqlcreateTable ="CREATE TABLE IF NOT EXISTS u_reg (
+id INT(6) AUTO_INCREMENT PRIMARY KEY, 
+name VARCHAR(20) NOT NULL,
+fname VARCHAR(20) NOT NULL,
+school VARCHAR(20),rollno VARCHAR(20),class VARCHAR(20)";
+mysqli_query($conn, $sqlcreateTable);
 ?>
